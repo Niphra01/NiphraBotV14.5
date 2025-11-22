f8db55671df7ac3aa8211f048ad2ee2496832d84
Niphra01 <Niphra01@gmail.com>
2025-11-22 18:31:44 +0300

    removed old codes

--- commands/games/rps.js
+++ commands/games/rps.js
@@ -57,63 +57,6 @@ module.exports = {
       else if (reason === 'over') {
       }
     })
-    //   const rpsR = await interaction.reply({ embeds: [embed], fetchReply: true });
-
-    //   await rpsR.react("🗻");
-    //   await rpsR.react("📄");
-    //   await rpsR.react("✂️");
-
-
-    //   const filter = (reaction, user) =>
-    //     ["🗻", "📄", "✂️"].includes(reaction.emoji.name) &&
-    //     user.id === interaction.user.id &&
-    //     !user.bot;
-
-    //   await rpsR
-    //     .awaitReactions({
-    //       filter,
-    //       max: 1,
-    //       time: 10000,
-    //     })
-    //     .then((collected) => {
-    //       const reaction = collected.first();
-    //       const emoji = reaction.emoji.name;
-    //       switch (emoji) {
-    //         case "🗻":
-    //           if (botChoice === "rock") {
-    //             interaction.editReply({ embeds: [embed.setDescription("It's a tie!")] });
-    //           } else if (botChoice === "paper") {
-    //             interaction.editReply({ embeds: [embed.setDescription("I win!")] });
-    //           } else {
-    //             interaction.editReply({ embeds: [embed.setDescription("You win!")] });
-    //           }
-    //           break;
-    //         case "📄":
-    //           if (botChoice === "paper") {
-    //             interaction.editReply({ embeds: [embed.setDescription("It's a tie!")] });
-    //           } else if (botChoice === "scissors") {
-    //             interaction.editReply({ embeds: [embed.setDescription("I win!")] });
-    //           } else {
-    //             interaction.editReply({ embeds: [embed.setDescription("You win!")] });
-    //           }
-    //           break;
-    //         case "✂️":
-    //           if (botChoice === "scissors") {
-    //             interaction.editReply({ embeds: [embed.setDescription("**It's a tie!**")] });
-    //           } else if (botChoice === "rock") {
-    //             interaction.editReply({ embeds: [embed.setDescription("**I win!**")] });
-    //           } else {
-    //             interaction.editReply({ embeds: [embed.setDescription("**You win!**")] });
-    //           }
-    //           break;
-    //         default:
-    //           interaction.editReply({ embeds: [embed.setDescription("**Times Up, You didn't choose anything.**")] });
-    //           break;
-    //       }
-    //     })
-    //     .catch((err) => {
-    //       interaction.editReply({ embeds: [embed.setDescription("**Times Up, You didn't choose anything.**")] });
-    //     });
   },
 };
 
